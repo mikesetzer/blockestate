77329c1b1f1551a19eb17f5f7fd7e2cd519a702a
Mike Setzer <michaelsetzerucf@gmail.com>
2024-03-31 13:29:45 -0400

    Set up testing framework and app name

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,35 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller, inspector, lender
+    let realEstate, escrow
+    
+    it('saves the addresses', async () => {
 
+        // Get signers from blockchain with HardHat & setup accounts
+        [buyer, seller, inspector, lender] = await ethers.getSigners()
+
+        // Deploy Rest Estate
+        const RealEstate = await ethers.getContractFactory('RealEstate')
+        realEstate = await RealEstate.deploy()
+
+        // Mint NFT using ipfs metadata
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+        await transaction.wait()
+        
+        const Escrow = await ethers.getContractFactory('Escrow')
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address
+        )
+
+        let result = await escrow.nftAddress()
+        expect(result).to.be.equal(realEstate.address)
+
+        result = await escrow.seller()
+        expect(result).to.be.equal(seller.address)
+        
+    })
 })
